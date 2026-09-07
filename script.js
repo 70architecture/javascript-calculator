d450d32263776e4312562ede5a138bfb29fef8aa
kondou naoto <kondounaoto@kondounoMacBook-Air.local>
2026-09-07 22:27:52 +0900

    最初のコミット

---
+++ script.js
@@ -0,0 +1,45 @@
+const numbers = [1, 2, 3];
+let operator = "";
+let firstNumber = 0;
+let secondNumber = 0;
+
+const numberButtons = document.querySelectorAll(".number");
+numberButtons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        if (operator === "") {
+            firstNumber = Number(button.textContent);
+            console.log("firstNumber:", firstNumber);
+        } else {
+            secondNumber = Number(button.textContent);
+            console.log("secondNumber:", secondNumber);
+        }
+
+        display.textContent = button.textContent;
+    });
+});
+const display = document.getElementById("display");
+const plusButton = document.getElementById("plus");
+const minusButton = document.getElementById("minus");
+const equalButton = document.getElementById("equal");
+
+plusButton.addEventListener("click", function() {
+    operator = "+";
+    console.log(operator);
+});
+minusButton.addEventListener("click", function() {
+    operator = "-";
+});
+equalButton.addEventListener("click", function() {
+    if (operator === "+") {
+        display.textContent = firstNumber + secondNumber;
+    }
+
+    if (operator === "-") {
+        display.textContent = firstNumber - secondNumber;
+    }
+});
+numberButtons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        display.textContent = button.textContent;
+    });
+});
